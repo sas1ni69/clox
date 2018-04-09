0b01d8d0cc24dff81281aae2f3b930d0af75af15
Hassanin Ahmed <sas1ni69@gmail.com>
2018-04-09 16:23:39 +0800

    adds constants

--- debug.c
+++ debug.c
@@ -10,9 +10,16 @@ void disassembleChunk(Chunk* chunk, const char* name) {
   }
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+  return offset + 2; // OP_CONSTANT is two (bytes) — one for the opcode and one for the operand.
+
 static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
-  return offset + 1;
+  return offset + 1; // Where OP_RETURN was only a single byte,
 }
 
 int disassembleInstruction(Chunk* chunk, int offset) {
@@ -21,6 +28,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   uint8_t instruction = chunk->code[offset];
 
   switch (instruction) {
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
--- debug.h
+++ debug.h
@@ -2,6 +2,7 @@
 #define clox_debug_h
 
 #include "chunk.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name);
 int disassembleInstruction(Chunk* chunk, int i);

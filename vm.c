a29f10273dc34bf7b7354e050b3ab865476e5443
Hassanin Ahmed <sas1ni69@gmail.com>
2018-04-16 19:15:58 +0800

    adds binary operators

--- vm.c
+++ vm.c
@@ -20,6 +20,12 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a  = pop(); \
+    push(a op b); \
+  } while (false)
 
   for(;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -39,6 +45,10 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
       case OP_NEGATE: {
         push(-pop());
         break;
@@ -53,6 +63,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
